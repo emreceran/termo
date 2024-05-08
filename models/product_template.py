84d739c33912cc2acf695c4e22a90c976d825ea9
ekaravanagd <ekaravanagd@gmail.com>
2024-05-08 19:53:02 +0300

    list view edit

--- models/product_template.py
+++ models/product_template.py
@@ -1,76 +1,196 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-from odoo import api, fields, models, _
-from odoo.addons.http_routing.models.ir_http import slug, unslug
-from odoo.addons.website.models import ir_http
-from odoo.tools.translate import html_translate
-from odoo.osv import expression
+from odoo import models, fields, api
+from odoo.exceptions import UserError, ValidationError, AccessError
+
+
+
 
 class ProductTemplate(models.Model):
-    _inherit = [
-        "product.template",
-        "website.seo.metadata",
-        'website.published.multi.mixin',
-        'website.searchable.mixin',
-        'rating.mixin',
-    ]
-    _name = 'product.template'
-    _mail_post_access = 'read'
-    _check_company_auto = True
+    _inherit = "product.template"
+
+
+    yuzey = fields.Float(string = "Yüzey", default = 0, store=True)
+    boru_hacmi = fields.Float(string = "Boru Hacmi", default = 0, store=True)
+    fan_adet = fields.Integer(string="Fan Adeti", default=0, store=True)
+    fan_cap = fields.Integer(string="Fan Çapı", default=0, store=True)
+
+    #EVAPOATÖR FİELDS
+    t1 = fields.Integer (string = "To / Te -35°C/-40°C W", default=None)
+    t2 = fields.Integer (string = "To / Te -40°C/-45°C W", default = 0, store=True)
+    sc1 = fields.Integer (string = "SC1 10°C/0°C W", default = 0, store=True)
+    sc2 = fields.Float (string = "SC2 0°C/- 8°C W", default = 0, store=True)
+    sc3 = fields.Integer (string = "SC3 - 18°C/ - 25°C W", default = 0, store=True)
+    sc4 = fields.Integer (string = "SC4 - 25°C/ - 31°C W", default = 0, store=True)
+    rezidans_t1 = fields.Char(string="REzidans T1 Batarya Coil")
+    rezidans_t2_batarya = fields.Char(string="REzidans T2 Batarya Coil")
+    rezidans_t2_tava = fields.Char(string="REzidans T2 Tva Drip Tray")
+    hava_debisi = fields.Integer(string = "Hava Debisi m³/h", default = 0, store=True)
+
+    #KONDENSER FİELDS
+    kapasiteyuksek_devir = fields.Float(string="Kapasite YÜKSEK DEVİR(H) ", default=0, store=True)
+    kapasite_dusuk_devir = fields.Float(string="KapasiteDÜŞÜK DEVİR (L) ", default=0, store=True)
+    hava_debisi_yuksek_devir = fields.Float(string="YÜKSEK DEVİR HAVA DEBİSİ m3/h", default=0, store=True)
+    hava_debisi_dusuk_devir = fields.Float(string="Düşük DEVİR HAVA DEBİSİ m3/h", default=0, store=True)
+    kondenser_giris_cap = fields.Char(string="KONDENSER  GİRİŞ  KOLLEKTÖR  ÇAPI mm / inc")
+    kondenser_cikis_cap = fields.Char(string="KONDENSER  ÇIKIŞ KOLLEKTÖR  ÇAPI mm / inc")
+
+
+    #ORTAK FİELDS
+    H = fields.Integer(string = "H", default = 0, store=True)
+    L = fields.Integer(string = "L", default = 0, store=True)
+    W = fields.Integer(string = "W", default = 0, store=True)
+    H1 = fields.Integer(string = "H1", default = 0, store=True)
+    H2 = fields.Integer(string = "H2", default = 0, store=True)
+    L1 = fields.Integer(string = "L1", default = 0, store=True)
+    W1 = fields.Integer(string = "W1", default = 0, store=True)
+    W2 = fields.Integer(string = "W2", default = 0, store=True)
+    W3 = fields.Integer(string = "W3", default = 0, store=True)
+    LA = fields.Integer(string = "LA", default = 0, store=True)
+
+    lamel_aralik= fields.Integer(string = "Lamel Aralığı", default = 4, store=True)
+    hatve= fields.Integer(string = "Hatve ", default = 6, store=True)
+    kapasite = fields.Integer(string = "Kapasite ", default = 6, store=True)
+
+
+    termo_tip_id = fields.Many2one('termo.tip', string="Ürün Tipi")
+    termo_filtre =fields.Many2many(related="termo_tip_id.filtre")
+
+
+    oda_sicakligi = fields.Float(string = "Oda Sıcaklığı ", compute="_value_pc", default = 6, store=True)
+    evaporasyon_sicakligi = fields.Float(string = "Evaporasyon Sıcaklığı ", compute="_value_pc", default = 6, store=True)
 
 
 
-    def _get_sales_prices(self, pricelist):
-        pricelist.ensure_one()
-        partner_sudo = self.env.user.partner_id
 
-        # Try to fetch geoip based fpos or fallback on partner one
-        fpos_id = self.env['website']._get_current_fiscal_position_id(partner_sudo)
-        fiscal_position = self.env['account.fiscal.position'].sudo().browse(fpos_id)
 
-        sales_prices = pricelist._get_products_price(self, 1.0)
+    @api.depends('hatve', 'sc2', 'sc3', 'sc4')
+    def _value_pc(self):
+        for record in self:
+            if record.hatve == 6:
+                record.kapasite = record.sc2
+                record.oda_sicakligi = 0
+                record.evaporasyon_sicakligi = 8
+            elif record.hatve == 8:
+                record.kapasite = record.sc3
+                record.oda_sicakligi = -18
+                record.evaporasyon_sicakligi = 25
+            elif record.hatve == 10:
+                record.kapasite = record.sc4
+                record.oda_sicakligi = -25
+                record.evaporasyon_sicakligi = 31
 
-        show_discount = pricelist.discount_policy == 'without_discount'
 
-        base_sales_prices = self.price_compute('list_price', currency=pricelist.currency_id)
-        print(base_sales_prices)
+    def gorev(self):
+        for record in self:
 
-        res = {}
-        for template in self:
-            price_reduce = sales_prices[template.id]
+            print(record.name)
+            a =record.termo_filtre #üründeki filtreler
 
-            product_taxes = template.sudo().taxes_id.filtered(lambda t: t.company_id == t.env.company)
-            taxes = fiscal_position.map_tax(product_taxes)
+            for attrib in a: #filtre isimleri loop ör sc1_aralik
 
-            price_reduce = self.env['account.tax']._fix_tax_included_price_company(
-                price_reduce, product_taxes, taxes, self.env.company)
 
-            tax_display = self.user_has_groups('account.group_show_line_subtotals_tax_excluded') and 'total_excluded' or 'total_included'
-            price_reduce = taxes.compute_all(price_reduce, pricelist.currency_id, 1, template, partner_sudo)[tax_display]
+                self.env['product.template.attribute.line'].search(
+                    [('product_tmpl_id', '=', record.id), ('attribute_id', '=', attrib.id)]).unlink()
+                referans = attrib.ref #atribute reeras
+                print(referans)
+                urun_sc1_degeri = getattr(record, referans)
+                urun_filtre_degeri = False
+                print([urun_sc1_degeri, attrib.delta_deger])
 
-            template_price_vals = {
-                'price_reduce': price_reduce
-            }
-            base_price = None
-            price_list_contains_template = price_reduce != base_sales_prices[template.id]
+                alt = int(urun_sc1_degeri - (urun_sc1_degeri % attrib.delta_deger))
+                ust = int(alt + attrib.delta_deger)
+                urun_filtre_degeri = str(alt) + " - " + str(ust)
 
-            if template.compare_list_price:
-                # The base_price becomes the compare list price and the price_reduce becomes the price
-                base_price = template.compare_list_price
-                if not price_list_contains_template:
-                    price_reduce = base_sales_prices[template.id]
-                    template_price_vals.update(price_reduce=price_reduce)
-            elif show_discount and price_list_contains_template:
-                base_price = base_sales_prices[template.id]
+                print(urun_filtre_degeri)
 
-            if base_price and base_price != price_reduce:
-                base_price = self.env['account.tax']._fix_tax_included_price_company(
-                    base_price, product_taxes, taxes, self.env.company)
-                base_price = taxes.compute_all(base_price, pricelist.currency_id, 1, template, partner_sudo)[
-                    tax_display]
-                template_price_vals['base_price'] = base_price
+                attrib_value = attrib.value_ids
+                print([x.name for x in attrib_value])
+                deger = [x for x in attrib_value if x.name == urun_filtre_degeri][0]
 
-            res[template.id] = template_price_vals
-        print(res)
-        return res
+
+                if deger:
+                    self.env['product.template.attribute.line'].create({'product_tmpl_id': record.id,
+                                                                        'attribute_id': attrib.id,
+                                                                        'value_ids': [(4, deger.id)],
+                                                                        })
+                else:
+                    raise UserError(
+                        'Değiştirmye çalıştığınız %s ürününün %s filtresinde yer alan  %s  değeri geçersiz. Kontrol ediniz yada filtre değerlerine ekleyiniz.' % ( record.name, attrib.name, deger))
+
+                # variant = self.env['product.attribute.value'].search(
+                #     [('attribute_id', '=', attr_id.id), ('name', '=', sc1_aralik_deger,)])
+                #
+                # if variant:
+                #     self.env['product.template.attribute.line'].create({'product_tmpl_id': record.id,
+                #                                                         'attribute_id': attr_id.id,
+                #                                                         'value_ids': [(4, variant.id)],
+                #                                                         })
+                # else:
+                #     raise UserError(
+                #         'Değiştirmye çalıştığınız %s ürününün %s filtresinde yer alan  %s  değeri geçersiz. Kontrol ediniz yada filtre değerlerine ekleyiniz.' % (
+                #         record.name, attrib_name, sc1_aralik_deger))
+
+
+
+    def tum_gorev(self):
+        raise UserError(self.termo_tip_id)
+
+        # tum_urunler = self.env['product.template'].search([])
+        # for record in tum_urunler:
+        #     if record.termo_tip_id != False:
+        #         record.gorev()
+
+
+    def gorev2(self):
+
+        attr_id = self.env['product.attribute'].search([('name', '=', 'sc1_aralik')])
+        values = self.env['product.attribute.value'].search([('attribute_id', '=', attr_id.id)])
+        value_names =   [x.name for x in values]
+        value_names =   [x.name for x in values if x.name=="0 - 5000"][0]
+        for record in self:
+            sc1_aralik = str(record.sc1_aralik)
+
+        prints =  value_names == sc1_aralik
+        print(value_names)
+        print(sc1_aralik)
+
+        raise UserError(prints)
+
+        attrib_name = "sc1_aralik"
+        attr_id = self.env['product.attribute'].search([('name', '=', attrib_name)])
+        self.env['product.template.attribute.line'].search(
+            [('product_tmpl_id', '=', record.id), ('attribute_id', '=', 93)]).unlink()
+
+
+    def gorev3(self):
+        a = self.termo_filtre
+        attribs = [x.name for x in a]
+        for i in attribs:
+
+            attrib_name = i
+            attr_id = self.env['product.attribute'].search([('name', '=', attrib_name)])
+
+            for record in self:
+
+                self.env['product.template.attribute.line'].search([('product_tmpl_id', '=', record.id), ('attribute_id', '=', attr_id.id)]).unlink()
+                sc1_aralik_deger = getattr(record, attrib_name)
+
+                if sc1_aralik_deger:
+                    variant = self.env['product.attribute.value'].search([('attribute_id', '=', attr_id.id), ('name', '=', sc1_aralik_deger,)])
+
+                    if variant:
+                        self.env['product.template.attribute.line'].create({'product_tmpl_id': record.id,
+                                                                            'attribute_id': attr_id.id,
+                                                                            'value_ids': [(4, variant.id)],
+                                                                            })
+                    else:
+                        raise UserError('Değiştirmye çalıştığınız %s ürününün %s filtresinde yer alan  %s  değeri geçersiz. Kontrol ediniz yada filtre değerlerine ekleyiniz.' % (record.name, attrib_name, sc1_aralik_deger))
+
+
+        # for record in self:
+        #     self.env['product.template.attribute.line'].search(
+        #     [('product_tmpl_id', '=', record.id), ('attribute_id', '=', attr_id.id)]).unlink()
+
+
+
